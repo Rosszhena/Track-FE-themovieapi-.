6f689423573b0dcbd60a05879ebb4bee296e2ec4
Maria Rosa <ross.zhena@gmail.com>
2018-08-24 01:37:37 -0500

    Se agrega modal

--- src/js/main.js
+++ src/js/main.js
@@ -14,8 +14,29 @@ btnWonderWoman.addEventListener("click", event => {
                 document.getElementById("galeria").innerHTML += "";
                 document.getElementById("galeria").innerHTML += `<div><h1>${pelicula.Title}</h1><img src="img/WonderWoman.jpg"></div>`}
             else{
-            document.getElementById("galeria").innerHTML += `<div><h1>${pelicula.Title}</h1><img src="
-            ${pelicula.Poster}"></div>`}
+            document.getElementById("galeria").innerHTML += `<div class="card">
+            <a href="#" data-toggle="modal" data-target="#id${pelicula.imdbID}">
+            <img src="${pelicula.Poster}" alt="" class="card-img-top"></a></div>
+            
+            <div class="modal fade" id="id${pelicula.imdbID}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+
+                    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                        
+                    <div class="modal-content">
+                        <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">${pelicula.Title}</h5>
+                        
+                        </div>
+                        <div class="modal-body">
+                                <img src="${pelicula.Poster}" alt="" class="img-fluid rounded">
+                        </div>
+                        
+                    </div>
+                    </div>
+                </div>`}
           });
     })
 });
@@ -34,8 +55,29 @@ btnBatman.addEventListener("click", event => {
                 
                 document.getElementById("galeria").innerHTML += `<div><h1>${pelicula.Title}</h1><img src="img/Batman.jpg"></div>`}
             else{
-            document.getElementById("galeria").innerHTML += `<div><h1>${pelicula.Title}</h1><img src="
-            ${pelicula.Poster}"></div>`}
+                document.getElementById("galeria").innerHTML += `<div class="card">
+                <a href="#" data-toggle="modal" data-target="#id${pelicula.imdbID}">
+                <img src="${pelicula.Poster}" alt="" class="card-img-top"></a></div>
+                
+                <div class="modal fade" id="id${pelicula.imdbID}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+    
+                        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                            
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">${pelicula.Title}</h5>
+                            
+                            </div>
+                            <div class="modal-body">
+                                    <img src="${pelicula.Poster}" alt="" class="img-fluid rounded">
+                            </div>
+                            
+                        </div>
+                        </div>
+                    </div>`}
           });
     })
 });
@@ -52,8 +94,29 @@ btnSpiderman.addEventListener("click", event => {
                 document.getElementById("galeria").innerHTML += "";
                 document.getElementById("galeria").innerHTML += `<div><h1>${pelicula.Title}</h1><img src="img/Spider-Man.jpg"></div>`}
             else{
-            document.getElementById("galeria").innerHTML += `<div><h1>${pelicula.Title}</h1><img src="
-            ${pelicula.Poster}"></div>`}
+            document.getElementById("galeria").innerHTML += `<div class="card">
+            <a href="#" data-toggle="modal" data-target="#id${pelicula.imdbID}">
+            <img src="${pelicula.Poster}" alt="" class="card-img-top"></a></div>
+            
+            <div class="modal fade" id="id${pelicula.imdbID}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+
+                    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                        
+                    <div class="modal-content">
+                        <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">${pelicula.Title}</h5>
+                        
+                        </div>
+                        <div class="modal-body">
+                                <img src="${pelicula.Poster}" alt="" class="img-fluid rounded">
+                        </div>
+                        
+                    </div>
+                    </div>
+                </div>`}
           });
     })
 });
