3285cfff1b3842536d83c7b4b7ba4a25d0761984
Maria Rosa Rosete <ross.zhena@gmail.com>
2018-11-28 23:07:31 -0600

    Se modifica url

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,9 @@
 window.getMovies = (api, search) => {
-return fetch(`http://www.omdbapi.com/?apikey=${api}&s=${search}&plot=full&type=movie`)
-.then(response =>{
-    return response.json()
-})
-.then(data =>{
-    return data
-})
+    return fetch(`https://www.omdbapi.com/?apikey=${api}&s=${search}&plot=full&type=movie`)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            return data
+        })
 }
-
-
-
-
